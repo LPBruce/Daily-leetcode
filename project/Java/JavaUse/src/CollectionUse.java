2de96853149e6f5942a020ead26656b2628c1c02
Peng Li <jylipeng@tju.edu.cn>
2021-07-28 23:44:32 +0800

    build java use project

---
+++ project/Java/JavaUse/src/CollectionUse.java
@@ -0,0 +1,13 @@
+public class CollectionUse {
+    public static void main(String[] args) {
+        queueUseCase();
+    }
+
+    // queue 使用总结
+    private static void queueUseCase() {
+        System.out.println("++++++++++++++++++++++Java queue usecase start+++++++++++++++++++++");
+
+
+        System.out.println("+++++++++++++++++++++++Java queue usecase end++++++++++++++++++++++");
+    }
+}
